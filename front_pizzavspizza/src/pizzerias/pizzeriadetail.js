6f1e2c958d34845cd9f915ed4163181ef3cc388a
romanandreev <anra@bk.ru>
2021-05-30 18:47:51 +0300

    React фиктивные данные

---
+++ front_pizzavspizza/src/pizzerias/pizzeriadetail.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class PizzaDetail extends  Component {
+    render() {
+    const p = this.props.p
+        return(
+            <div>
+                <h4>{p.id}</h4>
+                <h4>{p.pizzeria_name}</h4>
+                <h4>{p.city}</h4>
+                <h4>{p.zip_code}</h4>
+            </div>
+        )
+    }
+}
+export default PizzaDetail
