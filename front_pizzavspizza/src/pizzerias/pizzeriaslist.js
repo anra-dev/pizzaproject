b25d19c1c3f9411bc003f7da3cbc93cb0a2fd237
romanandreev <anra@bk.ru>
2021-05-30 19:39:08 +0300

    React забирает данные из API

--- front_pizzavspizza/src/pizzerias/pizzeriaslist.js
+++ front_pizzavspizza/src/pizzerias/pizzeriaslist.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import DummyData from './dummydata.json'
 import PizzaDetail from "./pizzeriadetail";
+import axios from 'axios';
+
 
 class PizzaList extends Component {
+    state = {
+        pizzeriasData:[],
+    }
+    componentDidMount(){
+        axios.get('http://localhost:8000')
+            .then((response) => {
+                this.setState({pizzeriasData: response.data});
+            })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
     render() {
         return(
             <div>
-                {DummyData.map( item => {
-                    return <PizzaDetail p = {item}/>
+                {this.state.pizzeriasData.map( item => {
+                    return <h3 key={item.id}>{item.pizzeria_name}, {item.city}</h3>
                 })
                 }
             </div>
